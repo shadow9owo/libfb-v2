171d03b182a23fa4503f6e4fe32fa2a77ddcaf6b
shadowdev <69159493+shadow9owo@users.noreply.github.com>
2025-03-19 18:32:07 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,63 +1,70 @@
 #include <fb.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 //custom libs
 #include "utils.h"
 #include "types.h"
 #include "config.h"
 
-extern int RenderCallbacks();
+extern int RenderCallbacks(FB *framebuffer);
 extern int HandleInput();
+
+bool Isrunning = true;
 
 int main(void)
 {
-	FB test;
-	if (RawCLI)
-	{
-		test = fb_init(GetScreenSize().X,GetScreenSize().Y,0,0);  //fullscreen
-	}else 
-	{
-		test = fb_init(0,0,0,0);
-	}
-	if (test)
-	{
-		if (Debug)
-		{
-			printf("Success\n"); //initilized successfully
-		}
-	}
- 	else
-	{
-		exit(-1);
-	}
-	fb_remove(test);
-	while (Isrunning)
-	{
-		 RenderCallbacks(test);
-		//infinite loop
-	}
-	return 0;
+    FB *test;
+    if (RawCLI)
+    {
+        test = fb_init(GetScreenSize().X, GetScreenSize().Y, 0, 0);  // fullscreen
+    }
+    else
+    {
+        test = fb_init(0, 0, 0, 0);
+    }
+
+    if (!test)
+    {
+        fprintf(stderr, "Failed to initialize framebuffer\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if (Debug)
+    {
+        printf("Framebuffer initialized successfully\n");
+    }
+
+    while (Isrunning)
+    {
+        RenderCallbacks(test);
+        HandleInput();
+    }
+
+    fb_remove(test);
+    return 0;
 }
 
 int HandleInput()
 {
-	while (1)
-	{
-			
-	}
-	Isrunning = false;
-	return 0;
+    return 0;
 }
 
-int RenderCallbacks(FB framebuffer)
+int RenderCallbacks(FB *framebuffer)
 {
-	FB test = framebuffer;
-	RECT *rect = fb_init_rect(100, 100, 200, 150, WHITE, test);
-	while (1)
-	{
-		fb_render(test);
-	}
-	fb_remove(test);
-	Isrunning = false;
-	return 0;
+    RECT *rect = fb_init_rect(100, 100, 200, 150, WHITE, framebuffer);
+    if (!rect)
+    {
+        fprintf(stderr, "Failed to create rectangle\n");
+        return -1;
+    }
+
+    while (Isrunning)
+    {
+        fb_render(framebuffer);
+    }
+
+    Isrunning = false;
+    return 0;
 }
